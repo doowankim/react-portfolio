ba14276e97da443f3c74e78530749473a2543958
doowankim <rlaendhks11@gmail.com>
2020-09-12 15:50:36 +0900

    global style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Router from './router';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #fff;
+  }
+`;
 
 const App = () => {
 	return (
 		<div>
+			<GlobalStyle />
 			<Router />
 		</div>
 	);
