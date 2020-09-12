f16199786a080cb7e9dd414c3bf897665480a19e
doowankim <rlaendhks11@gmail.com>
2020-09-12 16:43:00 +0900

    Header Style edit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,17 @@ import * as Styled from './styles';
 const Header = () => {
 	return (
 		<Styled.Container>
-			<Styled.Wrapper>Header</Styled.Wrapper>
+			<Styled.Wrapper>
+				<Styled.Logo>
+					<div>PORTFOLIO</div>
+				</Styled.Logo>
+				<Styled.Menu>
+					<li>HOME</li>
+					<li>ABOUT</li>
+					<li>CAREER</li>
+					<li>CONTACT</li>
+				</Styled.Menu>
+			</Styled.Wrapper>
 		</Styled.Container>
 	);
 };
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,11 +1,43 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
 	position: fixed;
 	width: 100%;
 	height: 50px;
+	background: #495057;
+	padding-left: 20px;
 `;
 
 export const Wrapper = styled.div`
-	background: #495057;
+	display: flex;
+	justify-content: center;
+	line-height: 50px;
+`;
+
+export const Logo = styled.div`
+	font-size: 36px;
+    color: #f9f9f9;
+    transition: 0.35s;
+    cursor: pointer;
+
+	&:hover {
+        background: ${darken(0.2, '#f9f9f9')}
+        transition: 0.35s;
+	}
+`;
+
+export const Menu = styled.div`
+	display: flex;
+	padding-left: 52px;
+	align-items: center;
+	flex: 1;
+
+	li {
+		display: inline-block;
+		font-size: 16px;
+		font-weight: bold;
+		margin-right: 16px;
+		cursor: pointer;
+	}
 `;
